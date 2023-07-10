45d7443bc4e705f6b6e55f885bb423acdb25531a
Chris-Effah <akboateng41@gmail.com>
2023-07-10 16:51:25 -0700

    magic-calculation

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,21 +7,21 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
-	listint_t *fast = list->next;
+	listint_t *fast = list;
 
-	if (list == NULL)
+	if (!list)
 	{
 		return (0);
 	}
 
 	while (fast && slow && fast->next)
 	{
+		slow = slow->next;
+		fast = fast->next->next;
 		if (slow == fast)
 		{
 			return (1);
 		}
-		slow = slow->next;
-		fast = fast->next->next;
 	}
 	return (0);
 }
