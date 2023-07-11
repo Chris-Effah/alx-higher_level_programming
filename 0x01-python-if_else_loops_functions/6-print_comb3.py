786b4e3e96465a8d2968380be29275c79344eded
Chris-Effah <akboateng41@gmail.com>
2023-07-11 23:37:19 +0000

    adds two integers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
-print()
+        if tens_digit == 8 and ones_digit == 9:
+            print("{}{}".format(tens_digit, ones_digit))
+        else:
+            print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
 
