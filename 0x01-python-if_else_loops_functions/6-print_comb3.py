5cb2fbc5e48b586339c21536945f3078cd51bb4f
Chris-Effah <akboateng41@gmail.com>
2023-07-11 21:33:15 +0000

    print combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit, 10):
-        if tens_digit != ones_digit:
-            print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
-print("\b\b\n")
+for tens_digit in range(9):
+    for ones_digit in range(tens_digit+1, 10):
+        print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
+print("{:02d}".format(89))
