8c948f36026869c54cb7995c18e5c529f2022bdc
Chris-Effah <akboateng41@gmail.com>
2023-07-11 22:37:01 +0000

    checked if a character is uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        uppercase_char = chr(ord(char) - 32) if ord(char) >= 97 
-                         and ord(char) <= 122 else char
+        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char 
         print("{}".format(uppercase_char), end="")
-    print()   
+    print()
