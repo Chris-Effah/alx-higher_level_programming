c4144ad5054e3cbec4a5cc893dd93dd17fd847c2
Chris-Effah <akboateng41@gmail.com>
2023-07-12 05:10:28 +0000

    pycodestyle validation

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for char_code in range(ord('z'), ord('a') -1, -1):
-    print("{}".format(chr(char_code)), end="")
+for char_code in range(ord('Z'), ord('A') - 1, - 1):
+    print("{}".format(chr(char_code).lower() if char_code % 2 == 1
+                    else chr(char_code).upper()), end="")
