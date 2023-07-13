ee9d9deef590a9deffed5e97bfcdfd285936810a
Chris-Effah <akboateng41@gmail.com>
2023-07-13 15:57:11 -0700

    a script that adds arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-
-
     def print_arguments(argv):
         num_args = len(argv)
         if num_args == 0:
             print("0 arguments.")
             return
-
         print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
         for i, arg in enumerate(argv):
-           print(f"{i + 1}: {arg}")
+            print(f"{i + 1}: {arg}")
+
 if __name__ == "__main__":
-        print_arguments(sys.argv[1:])
+         print_arguments(sys.argv[1:])
