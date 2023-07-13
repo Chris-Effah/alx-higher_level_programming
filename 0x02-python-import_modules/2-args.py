1c50c57f3dd9a3e484539e81f1e1f9ec3695c951
Chris-Effah <akboateng41@gmail.com>
2023-07-13 14:53:19 -0700

    a script that prints arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
+
+
     def print_arguments(argv):
         num_args = len(argv)
         if num_args == 0:
@@ -11,4 +13,4 @@ if __name__ == "__main__":
         for i, arg in enumerate(argv):
            print(f"{i + 1}: {arg}")
 if __name__ == "__main__":
-        print_arguments(sys.argv[1: ])
+        print_arguments(sys.argv[1:])
