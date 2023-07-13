af1d3a35b7d5b9ff4a9068b6bbe7ae5a1aecb488
Chris-Effah <akboateng41@gmail.com>
2023-07-13 15:22:47 -0700

    a script that adds arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-    sum_of_args = 0
+    total = 0
     for i in range(len(sys.argv) - 1):
-        sum_of_args += int(sys.argv[i + 1])
-    print("{}".format(sum_of_args))
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
 
