0b9e00bfa30f68563c2b90090b8666a5728a7454
Chris-Effah <akboateng41@gmail.com>
2023-07-13 22:27:49 -0700

    added an ommited colon after an if statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,12 @@ import sys
 if __name__ == "__main__":
     def print_arguments(argv):
         num_args = len(argv)
+
         if num_args == 0:
             print("0 arguments.")
             return
         print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+
         for i, arg in enumerate(argv):
             print(f"{i + 1}: {arg}")
 
