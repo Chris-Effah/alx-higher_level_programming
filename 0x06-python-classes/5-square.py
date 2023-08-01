23b73b17120ed666d88c8e77b3f31e38c912657b
Chris-Effah <akboateng41@gmail.com>
2023-08-01 11:07:20 -0700

    fixed an indentation errors

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,11 +10,12 @@ class Square:
 
     @property
     def size(self):
+        """retrieve the current size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not is instance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
@@ -22,3 +23,9 @@ class Square:
 
     def area(self):
         return self.__size ** 2
+
+    def my_print(self):
+        for _ in range(self.__size):
+            print("#" * self.__size)
+        if self.__size == 0:
+            print()
