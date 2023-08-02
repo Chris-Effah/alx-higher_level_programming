247ae9342454d9cd2c9a7bdbe8daca0f003d3b7c
Chris-Effah <akboateng41@gmail.com>
2023-08-01 23:34:28 -0700

    f

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -34,7 +34,6 @@ class Node:
         self.__next_node = value
 
 
-
 class SinglyLinkedList:
     """represents a singly linked list"""
 
@@ -54,7 +53,8 @@ class SinglyLinkedList:
             self.__head = new_node
         else:
             current_node = self.__head
-            while current_node.next_node is not None and current_node.next_node.data < value:
+            while current_node.next_node is not None and
+            current_node.next_node.data < value:
                 current_node = current_node.next_node
             new_node.next_node = current_node.next_node
             current_node.next_node = new_node
