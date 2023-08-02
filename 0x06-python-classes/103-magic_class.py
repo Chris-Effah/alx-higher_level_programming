726524cfcc158f4849c0a568a01df42613a56f25
Chris-Effah <akboateng41@gmail.com>
2023-08-02 00:02:42 -0700

    f

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,7 @@ class MagicClass:
     """represents a circle"""
 
     def __init___(self, radius=0):
-        """initialzing a new magicclass"""
+        """initialzing a new MagicClass"""
 
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
