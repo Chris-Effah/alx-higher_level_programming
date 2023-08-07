6a1928743a4192b5bce5dcca10767351bc6a145a
Chris-Effah <akboateng41@gmail.com>
2023-08-07 16:46:32 -0700

    an empty class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,5 +45,5 @@ class Rectangle:
     def perimeter(self):
         return 2 * (self.__width + self.__height)
 
-        if width or height == 0:
+        if self.__width or self.__height == 0:
             self.perimeter = 0
