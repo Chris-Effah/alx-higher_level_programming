5e5773b101e728af7b2be2436a871d308ddc384e
Chris-Effah <akboateng41@gmail.com>
2023-08-07 18:31:07 -0700

    rectangle

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens(N):
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve_util(board, 0, N, solutions)
+    return solutions
+
+def solve_util(board, row, N, solutions):
+    if row == N:
+        solutions.append(board)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solve_util(board, row + 1, N, solutions)
+            board[row][col] = 0
+
+def print_solutions(solutions):
+    for board in solutions:
+        for row in board:
+            print(" ".join("Q" if cell == 1 else "." for cell in row))
+        print()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(N)
+    print_solutions(solutions)
+
+if __name__ == "__main__":
+    main()
+
