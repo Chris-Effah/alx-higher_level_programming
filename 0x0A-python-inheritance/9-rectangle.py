2a8430a08b07ef7f84d0c9c1e039fd50bf09e775
Chris-Effah <akboateng41@gmail.com>
2023-08-14 15:32:52 -0700

    inheritance

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""An class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a subclass of BaseGeometry"""
+
+    def __init__(self, width, height):
+        """instantiation with width and height"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """implementing the area method"""
+        return self.__width * self.__height
+
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
