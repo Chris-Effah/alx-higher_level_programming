811f198b0f4d5f894431940ffe7571f687f4bf9c
Chris-Effah <akboateng41@gmail.com>
2023-08-14 11:51:21 -0700

    inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a function - is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    this function that prints the list, but sorted
+
+    Parameters:
+    obj
+    a_class
+
+    Returns:
+    True if the object is an instance of
+    """
+    return isinstance(obj, a_class)
