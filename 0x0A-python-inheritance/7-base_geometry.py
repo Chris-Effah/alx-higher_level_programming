dbbb3e4643b3adcfda8e1dc0289c2db776467c74
Chris-Effah <akboateng41@gmail.com>
2023-08-14 15:14:04 -0700

    inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""An class BaseGeometry"""
+"""A class BaseGeometry"""
 
 
-class BaseGeometry:
+class BaseGeometry():
     """an area method"""
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """validates the value"""
         if not is instance(value, int):
             raise TypeError(f"{name} must be an integr")
         if value <= 0:
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,14 +2,14 @@
 """An class BaseGeometry"""
 
 
-class BaseGeometry:
+class BaseGeometry():
     """an area method"""
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integr")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
@@ -18,9 +18,7 @@ class Rectangle(BaseGeometry):
     """a subclass of BaseGeometry"""
     def __init__(self, width, height):
         """instantiation with width and height"""
-        self.__width = 0
-        self.__height = 0
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
