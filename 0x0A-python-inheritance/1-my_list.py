4357d5d85ddc0e8f616cc495dcab9b9c47afc5c1
Chris-Effah <akboateng41@gmail.com>
2023-08-14 11:38:30 -0700

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""a subclass"""
 
+
+class MyList(list):
+    """a function print_sorted"""
+
+
+    def print_sorted(self):
+        """
+        this function that prints the list, but sorted
+
+        Parameters:
+        self
+
+        Returns:
+        a sorted list
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
