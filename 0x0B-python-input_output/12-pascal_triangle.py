b6c20a4f92dd8424b3129dbec48b1dc6639b00ad
Chris-Effah <akboateng41@gmail.com>
2023-08-16 20:49:58 -0700

    input or output

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""pascaltriangle"""
+
+
+def pascal_triangle(n):
+    """a function that returns a lists of lists representing pascal's triangle
+    """
+    if n <= 0:
+        return []
+
+    tri = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = tri[i -1][j - 1] + tri[i - 1][j]
+        tri.append(row)
+    return tri
