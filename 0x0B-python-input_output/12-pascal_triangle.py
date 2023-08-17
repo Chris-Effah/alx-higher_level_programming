de85b83655376d1708ac861763b0f22703f88a74
Chris-Effah <akboateng41@gmail.com>
2023-08-16 22:31:04 -0700

    pascals triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,6 +13,6 @@ def pascal_triangle(n):
         row = [1] * (i + 1)
         if i > 1:
             for j in range(1, i):
-                row[j] = tri[i -1][j - 1] + tri[i - 1][j]
+                row[j] = tri[i - 1][j - 1] + tri[i - 1][j]
         tri.append(row)
     return tri
