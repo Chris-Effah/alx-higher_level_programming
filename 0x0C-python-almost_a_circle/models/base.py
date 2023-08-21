b2a554592aa214ca407abad5673c7fd08d072748
Chris-Effah <akboateng41@gmail.com>
2023-08-21 11:38:24 -0700

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""my first calss base"""
+
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """initializing a class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
