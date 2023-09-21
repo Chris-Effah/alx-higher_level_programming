81d3a2250187388141dba6e457d3a35810c2d5f7
Chris-Effah <akboateng41@gmail.com>
2023-09-20 21:45:33 -0700

    l

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT UNIGUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL, PRIMARY KEY(id)
 );
