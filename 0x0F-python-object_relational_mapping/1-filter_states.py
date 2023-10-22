ada890cd495a3b6f0b0a07fe746f2f086635ec00
Chris-Effah <akboateng41@gmail.com>
2023-10-21 22:24:01 -0700

    restarted MySQL

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a python script that lists all states with a name starting with N from db using ORM
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            password=mysql_password,
+            db=database_name
+    )
+
+    cursor = db.cursor()
+    query = "SELECT *FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
