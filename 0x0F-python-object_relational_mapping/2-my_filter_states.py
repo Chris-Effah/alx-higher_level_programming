f910e06a811660d9cd0808cc5feca2770c8450d8
Chris-Effah <akboateng41@gmail.com>
2023-10-22 11:37:44 -0700

    fixed an indentation error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                 .format(sys.argv[4]))
     states = cursor.fetchall()
     for state in states:
