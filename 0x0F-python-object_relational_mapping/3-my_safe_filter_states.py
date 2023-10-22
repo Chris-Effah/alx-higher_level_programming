50fade5bb7f4491322e83777912fffade406b796
Chris-Effah <akboateng41@gmail.com>
2023-10-22 13:52:47 -0700

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+a script that lists all values in the states from db using ORM
+"""
+
+import MySQLdb
+import sys
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    cursor.execute(query, (state_name,))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
