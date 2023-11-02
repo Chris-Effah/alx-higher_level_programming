310bfaacdb8bca43cbfa6fc468a9b84e44f17cb2
Chris-Effah <akboateng41@gmail.com>
2023-11-02 16:22:12 -0700

    delete request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # a script that that takes in a URL, sends a POST request to the passed URL
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
