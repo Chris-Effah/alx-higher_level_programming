60763c5863f3f9c6153aadf310afd9fd2c3e87aa
Chris-Effah <akboateng41@gmail.com>
2023-11-05 15:02:31 -0800

    email POST

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import requests
+
+response = requests.get('https://alx-intranet.hbtn.io/status')
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
