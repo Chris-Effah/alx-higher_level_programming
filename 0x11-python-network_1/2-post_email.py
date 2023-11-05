10d09399a198d6788c0c52728c411388b751a106
Chris-Effah <akboateng41@gmail.com>
2023-11-05 14:35:36 -0800

    email POST

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import urllib.request
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python script.py <URL> <email>")
+    sys.exit(1)
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+with urllib.request.urlopen(url, data=data) as response:
+    body = response.read().decode('utf-8')
+print(body)
