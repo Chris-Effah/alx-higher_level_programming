ea936ec0d142849aed5b8d2fc8bb1b514f39d2c6
Chris-Effah <akboateng41@gmail.com>
2023-11-05 14:53:23 -0800

    email POST

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,6 +3,10 @@
 import urllib.request
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
 try:
     with urllib.request.urlopen(sys.argv[1]) as response:
         body = response.read().decode('utf-8')
